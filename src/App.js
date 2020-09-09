67e3ac2182c3a266189a413594747a8b76f6ddd7
HSHko <pleasmin@gmail.com>
2020-09-10 07:21:58 +0900

    Practice 20.09.10

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
-function App() {
+const Circle = styled.div`
+  width: 5rem;
+  height: 5rem;
+  background: ${props => props.color || 'black'};
+  border-radius: 50%;
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Circle />
   );
-}
+};
 
 export default App;
